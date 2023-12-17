16f36b1f9a61ba706be17b34beeca6215ee515e7
Seinly <macbookpro@MacBooks-MacBook-Pro.local>
2023-12-18 01:41:15 +0700

    Added API Docs

---
+++ API Docs.js
@@ -0,0 +1,35 @@
+/*
+
+API Docs By : Muhammad Seinly Ashviya
+
+http://localhost:3000/api/v1/admin (GET) ==> Get all admin
+http://localhost:3000/api/v1/admin/:id (GET) ==> Get admin by id
+http://localhost:3000/api/v1/admin (POST) ==> Create admin
+http://localhost:3000/api/v1/admin/:id (PUT) ==> Edit admin
+http://localhost:3000/api/v1/admin/:id (DELETE) ==> Delete admin
+
+http://localhost:3000/api/v1/supplier (GET) ==> Get all supplier
+http://localhost:3000/api/v1/supplier/:id (GET) ==> Get supplier by id
+http://localhost:3000/api/v1/supplier (POST) ==> Create supplier
+http://localhost:3000/api/v1/supplier/:id (PUT) ==> Edit supplier
+http://localhost:3000/api/v1/supplier/:id (DELETE) ==> Delete supplier
+
+http://localhost:3000/api/v1/pelanggan (GET) ==> Get all pelanggan
+http://localhost:3000/api/v1/pelanggan/:id (GET) ==> Get pelanggan by id
+http://localhost:3000/api/v1/pelanggan (POST) ==> Create pelanggan
+http://localhost:3000/api/v1/pelanggan/:id (PUT) ==> Edit pelanggan
+http://localhost:3000/api/v1/pelanggan/:id (DELETE) ==> Delete pelanggan
+
+http://localhost:3000/api/v1/transaksi (GET) ==> Get all transaksi
+http://localhost:3000/api/v1/transaksi/:id (GET) ==> Get transaksi by id
+http://localhost:3000/api/v1/transaksi (POST) ==> Create transaksi
+http://localhost:3000/api/v1/transaksi/:id (PUT) ==> Edit transaksi
+http://localhost:3000/api/v1/transaksi/:id (DELETE) ==> Delete transaksi
+
+http://localhost:3000/api/v1/barang (GET) ==> Get all barang
+http://localhost:3000/api/v1/barang/:id (GET) ==> Get barang by id
+http://localhost:3000/api/v1/barang (POST) ==> Create barang
+http://localhost:3000/api/v1/barang/:id (PUT) ==> Edit barang
+http://localhost:3000/api/v1/barang/:id (DELETE) ==> Delete barang
+
+*/
